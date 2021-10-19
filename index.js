1f72ec332ec77d1590e4bd03114ecade1f61c3d3
NourAybak <nooreldin3000@gmail.com>
2021-10-19 22:20:31 +0200

    add to TODO

--- index.js
+++ index.js
@@ -2,17 +2,20 @@
 
 const request = require("request")
 const cheerio = require("cheerio")
-// const targetSite = "https://keep.google.com"
-// const targetNode = ".PvRhvb-ibnC6b-V67aGc"
-const targetSite = "https://www.analyticsvidhya.com/blog/2020/10/web-scraping-using-node-js/"
-const targetNode = "h4"
+const gKeep = "https://keep.google.com"
+const gKeepNode = ".PvRhvb-ibnC6b-V67aGc"
 
-request(targetSite, (error, response, html) => {
+const   freecoursesite = "https://www.freecoursesite.com",
+        fcc = 'https://www.freecodecamp.org/'
+;
+const siteXNode = "h1"
+
+request(fcc, (error, response, html) => {
     if(!error && response.statusCode==200) {
         const $= cheerio.load(html);
         // const output= datarow.find("th").text();
 
-        $(targetNode).each((i, data) => {
+        $(siteXNode).each((i, data) => {
             const item= $(data).text();
 
             console.log(item);
